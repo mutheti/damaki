a1ed354c70f1dadfe93fbdd514c0405a6f081f56
mutheti <114909760+mutheti@users.noreply.github.com>
2025-08-28 17:45:41 +0300

    Update api.ts

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -12,7 +12,7 @@ export async function fetchFromApi<T>(
   try {
     // Ensure no double slashes
     const cleanEndpoint = endpoint.replace(/^\/+|\/+$/g, '');
-    const apiUrl = `http://localhost:4000/api/v1/${cleanEndpoint}`;
+    const apiUrl = `https://damaki.onrender.com/api/v1/${cleanEndpoint}`;
 
     // Get token from localStorage
     const token = typeof window !== 'undefined' ? localStorage.getItem('token') : null;
